e9bb7adfccbf7efaec421841ac4ed056451adbc6
Darren Rainey <darren@darrenraineys.co.uk>
2023-01-07 01:50:14 +0000

    Complete rewrite.

--- Printers/Get-Printer-IP-And-Mac-Address.ps1
+++ Printers/Get-Printer-IP-And-Mac-Address.ps1
@@ -1,3 +1,3 @@
 # https://github.com/DarrenRainey/qsdt
 # Attempts to retrieve the MAC address/serial number for all printers via ARP table cache on the current machine.
-GWMI -Class WIN32_Printer | Where-Object {$_.Location -ne $null} | % {new-object psobject -property @{Printer=$_.Name; IP=$_.Location; MAC=(Get-NetNeighbor -IPAddress $_.Location)} }
+Get-Printer |Where-Object {$_.PortName -notlike "WSD*" -and $_.PortName -notlike "PORTPRO*" -and $_.PortName -notlike "USB*" -and $_.PortName -notlike "*FAX*" -and $_.PortName -notlike "*Microsoft*"}| % {new-object psobject -property @{Printer=$_.Name; IP=$_.PortName; MAC=(Get-NetNeighbor -IPAddress $_.PortName).LinkLayerAddress} }
