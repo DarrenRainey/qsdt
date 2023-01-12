2d2de586391ad7717434d3c8f2759fcd62f72083
Darren Rainey <darren@darrenraineys.co.uk>
2023-01-12 14:59:28 +0000

    Update now adds hostname for each station as well as sorting.

--- Machine Tools/Get-Connected-SMB-Users.ps1
+++ Machine Tools/Get-Connected-SMB-Users.ps1
@@ -1,3 +1,9 @@
 # https://github.com/DarrenRainey/qsdt/
 # Gets all users how have open files on the current SMB file server.
-Get-SmbOpenFile | select ClientComputerName,ClientUsername -Unique
+function getHost($ip){
+	try{
+	return [System.Net.Dns]::GetHostEntry($ip).HostName
+	} catch {}
+}
+
+Get-SmbOpenFile | select ClientComputerName,ClientUsername -Unique| % {new-object psobject -property @{Username=$_.ClientUsername; IP=$_.ClientComputerName; Host=(getHost($_.ClientComputerName))} }
