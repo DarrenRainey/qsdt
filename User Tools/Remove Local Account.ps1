a7555459ae51fa4d06f3a935ec6898da13d5f9e6
Darren Rainey <darren@darrenraineys.co.uk>
2023-02-19 21:51:30 +0000

    Remove redundant code

--- User Tools/Remove Local Account.ps1
+++ User Tools/Remove Local Account.ps1
@@ -6,7 +6,6 @@ for ($i=1;$i -le $count.count; $i++)
 { Write-Host "$i. $($accounts[$i-1].name),$($accounts[$i-1].status)" 
 $menu.Add($i,($accounts[$i-1].name))}
 
-echo $menu
 [int]$ans = Read-Host 'Enter selection'
 $selection = $menu.Item($ans)
 Remove-LocalUser -Name $selection 
